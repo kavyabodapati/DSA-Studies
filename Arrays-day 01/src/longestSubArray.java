ddb3557e65b05cef3f51385c312a26f07caab471
kavyabodapati <71720164+kavyabodapati@users.noreply.github.com>
2025-05-03 21:39:48 +0530

    sixth day

---
+++ Arrays-day 01/src/longestSubArray.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class longestSubArray {
+//    public static int longsarray(int[] nums,int k) {
+//        int n = nums.length;
+//        long sum=0;
+//        int maxlen=0;
+//        Map<Long,Integer> sa=new HashMap<>();
+//        for(int i=0;i<n;i++){
+//            sum+=nums[i];
+//        }
+//    }
+    public static int 
+    public static void main(String[] args) {
+
+    }
+}
